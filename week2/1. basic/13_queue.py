db42a6e55f41b91dfeebddec6a94c648350cb4a2
woojin <woojinnj@naver.com>
2026-08-28 20:53:52 +0900

    13번은 뭐하는건지 잘 모르겠음

--- week2/1. basic/13_queue.py
+++ week2/1. basic/13_queue.py
@@ -26,6 +26,7 @@
 
 from collections import deque
 
+
 def process_print_queue(jobs):
     """
     프린터 작업을 순서대로 처리
@@ -37,15 +38,15 @@ def process_print_queue(jobs):
         처리된 작업 리스트
     """
     # TODO: deque로 큐 생성
-    queue = deque(jobs)
+    queue = deque(jobs) 
     
     processed = []
     
     # TODO: 큐가 비어있지 않은 동안 반복
     ## 큐에서 작업 꺼내기
     ## 작업 처리 (출력 및 리스트에 추가)
-    pass
-    
+    while queue:
+        processed.append(queue.popleft())
     return processed
 
 # 테스트 케이스
