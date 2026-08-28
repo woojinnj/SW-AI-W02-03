f3ac76fa0abe3e3beb9ff02eb1042e15ba38bb60
woojin <woojinnj@naver.com>
2026-08-28 19:30:16 +0900

    중간에 유클리드 확장 파트 잘 모르겠음

--- week2/1. basic/09_number_theory.py
+++ week2/1. basic/09_number_theory.py
@@ -23,7 +23,7 @@
 - 유클리드 호제법: gcd(a, b) = gcd(b, a % b)
 - LCM 공식: lcm(a, b) = (a × b) / gcd(a, b)
 """
-
+import math
 def gcd(a, b):
     """
     유클리드 호제법을 사용한 최대공약수 계산
@@ -37,7 +37,11 @@ def gcd(a, b):
     # TODO: 유클리드 호제법 구현
     # base case: b가 0이면 a 반환
     # recursive를 이용 
-    pass
+    if b==0:
+        return a
+    return gcd(b,a%b)
+
+    
 
 def gcd_iterative(a, b):
     """
@@ -51,8 +55,13 @@ def gcd_iterative(a, b):
     """
     # TODO: 반복문으로 구현
     # b가 0이 될 때까지 반복
-    pass
+    while(b!=0):
+        temp=b
+        b=a%b
+        a=temp
+    return a
 
+        
 def lcm(a, b):
     """
     최소공배수 계산
@@ -64,7 +73,11 @@ def lcm(a, b):
         최소공배수
     """
     # TODO: LCM 계산
-    pass
+    num=max(a,b)
+    while(1):
+        if num%a==0 and num%b==0:
+            return num
+        num+=1
 
 def extended_gcd(a, b):
     """
@@ -81,7 +94,15 @@ def extended_gcd(a, b):
     # base case: b가 0이면 (a, 1, 0) 반환    
     # recursive case
     # 역추적하며 x, y 계산
-    pass
+    if b == 0:
+        return a, 1, 0
+
+    g, x1, y1 = extended_gcd(b, a % b)
+
+    x = y1
+    y = x1 - (a // b) * y1
+
+    return g, x, y
 
 def is_prime(n):
     """
@@ -97,8 +118,15 @@ def is_prime(n):
     # n이 2보다 작으면 False
     # 2부터 sqrt(n)까지 나누어 떨어지는지 확인    
     # 3부터 sqrt(n)까지 홀수만 확인
-    pass 
+    if n<2:
+        return False
+    for i in range(2,int(math.sqrt(n))+1):
+        if n%i==0:
+            return False
+    return True
 
+
+        
 # 테스트 케이스
 if __name__ == "__main__":
     # 테스트 케이스 1: GCD와 LCM
