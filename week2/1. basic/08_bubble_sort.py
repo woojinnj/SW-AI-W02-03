abbd4b8bc5fcaf5d8894d32b11b89e661b5e8cdd
woojin <woojinnj@naver.com>
2026-08-28 17:16:29 +0900

    사소한오류수정

--- week2/1. basic/08_bubble_sort.py
+++ week2/1. basic/08_bubble_sort.py
@@ -42,7 +42,7 @@ def bubble_sort(arr):
     ## arr[j] > arr[j+1]이면 교환
     ## 외부 반복문: n-1번 실행
     for i in range(n-1):
-        for j in range(0,n-i-1):
+        for j in range(n-i-1):
             if arr[j]>arr[j+1]:
                 arr[j],arr[j+1]=arr[j+1],arr[j]
         
