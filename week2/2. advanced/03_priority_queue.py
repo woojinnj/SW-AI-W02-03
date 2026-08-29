40e21d08badd44494b70d0f7a7ae662692a9cda0
woojin <woojinnj@naver.com>
2026-08-29 13:22:07 +0900

    w2-advancde3 solved. 힙의 문법적 + 특성에 대해서 이해 필요한 문제

--- week2/2. advanced/03_priority_queue.py
+++ week2/2. advanced/03_priority_queue.py
@@ -41,14 +41,19 @@ def process_emergency_room(patients):
     
     
     # TODO: 모든 환자를 힙에 추가
-    pass
-        
+    for name, priority in patients:
+        heapq.heappush(heap,(priority,name))
+
+    # print(heap)
     processed = []
     
     # TODO: 힙이 비어있지 않은 동안 반복
     ## 힙에서 우선순위가 가장 높은 환자 꺼내기
     ## 환자 처리
-    pass
+    while heap:
+        priority,name=heapq.heappop(heap)
+        print(f"처리: {name} (우선순위: {priority})")
+        processed.append(name)
         
     return processed
 
