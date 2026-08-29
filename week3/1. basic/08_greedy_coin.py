7a4349de76864565e782a2f9da7598a777c3695e
woojin <woojinnj@naver.com>
2026-08-29 22:00:23 +0900

    3주차 basic solved

--- week3/1. basic/08_greedy_coin.py
+++ week3/1. basic/08_greedy_coin.py
@@ -46,8 +46,12 @@ def make_change_greedy(change, coins):
     # TODO: 각 동전에 대해 반복
     ## 현재 동전으로 거슬러줄 수 있는 개수 계산    
     ## 개수가 0보다 크면 결과에 추가
-    pass
-    
+    for i in coins:
+        if change//i>0:
+            result[i]=change//i
+        total_coins+=change//i
+        change=change%i
+
     return total_coins, result
 
 # 테스트 케이스
--- week3/1. basic/09_greedy_meeting.py
+++ week3/1. basic/09_greedy_meeting.py
@@ -33,19 +33,25 @@ def select_meetings(meetings):
         (배정된 회의 개수, 선택된 회의 리스트)
     """
     # TODO: 회의가 없으면 0 반환
-    pass
+    if not meetings:
+        return 0
     
     # TODO: 종료 시간 기준으로 정렬
-    pass
+    meetings.sort(key=lambda x:x[1])
+    # print(meetings)
+
     
     selected = []
     
     # TODO: 첫 번째 회의 선택
-    pass
+    selected.append(meetings[0])
+    # print(selected)
     
     # TODO: 나머지 회의들 확인
     ## 이전 회의가 끝난 후 시작하는 회의만 선택
-    pass
+    for i in meetings[1:]:
+        if i[0]>=selected[-1][1]:
+            selected.append(i)
     
     return len(selected), selected
 
