965aa91fbf9ac803e3d9a33b997a0ded8a69102c
woojin <woojinnj@naver.com>
2026-08-31 14:44:12 +0900

    w3advanced2 solved 했는데 모르겠어서 정답 찾아본거

--- week3/2. advanced/02_lcs.py
+++ week3/2. advanced/02_lcs.py
@@ -37,10 +37,22 @@ def lcs_length(s1: str, s2: str) -> int:
     어느 한쪽이라도 비어 있으면 0 을 반환합니다.
     """
     # TODO: 빈 문자열 처리
+    if not s1 or not s2:
+        return 0
     # TODO: (len(s1)+1) x (len(s2)+1) 크기의 2차원 dp 배열을 0 으로 초기화
+    dp = [[0] * (len(s2) + 1) for _ in range(len(s1) + 1)]
     # TODO: 이중 반복문으로 점화식에 따라 dp 채우기
+    for i in range(1, len(s1) + 1):
+        for j in range(1, len(s2) + 1):
+            if s1[i - 1] == s2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j]=max(
+                    dp[i-1][j],
+                    dp[i][j-1]
+                )
     # TODO: dp[len(s1)][len(s2)] 반환
-    pass
+    return dp[len(s1)][len(s2)]
 
 
 if __name__ == "__main__":
